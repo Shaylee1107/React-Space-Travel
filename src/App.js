3810674a6b23da4e1d660e00e83c7ca2ec3bc682
Owner <shayleebaumgartner2002@gmail.com>
2024-06-11 18:44:12 -0400

    adding content to Motto component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import NavigationBar from "./routes/NavigationBar";
+import AppRoute from "./routes/AppRoute";
+import Motto from "./components/Motto";
+import Loading from "./components/Loading";
 import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
-    <NavigationBar />
+    <BrowserRouter>
+      <NavigationBar />
+      <AppRoute />
+      <Motto />
+      {/* <Loading /> */}
+    </BrowserRouter>
   );
 }
 
