c7e24300d7ebb8d5a21ddc8e49634c131cd5157f
Owner <shayleebaumgartner2002@gmail.com>
2024-06-13 17:26:19 -0400

    making a SpacecraftProvider and wrapping all AppRoutes

---
+++ src/providers/SpacecraftProvider.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react';
+import SpacecraftContext from '../context/SpacecraftContext';
+import SpaceTravelApi from '../services/SpaceTravelApi';
+
+const SpacecraftProvider = ({ children }) => {
+    const INITIAL_STATE = [];
+    const [spacecrafts, setSpacecrafts] = useState(INITIAL_STATE);
+    const buildSpacecraft = (formData) => {
+      setSpacecrafts(crafts => [...crafts, formData]);
+  }
+
+  useEffect(() => {
+    if(spacecrafts.length > 0){
+        const newestSpacecraft = spacecrafts[spacecrafts.length - 1]; 
+        SpaceTravelApi.buildSpacecraft(newestSpacecraft);
+    }
+  }, [buildSpacecraft, spacecrafts])
+    return (
+        <>
+            <SpacecraftContext.Provider value={{spacecrafts, setSpacecrafts, buildSpacecraft}}>
+                {children}
+            </SpacecraftContext.Provider>
+        </>
+    )
+}
+
+export default SpacecraftProvider;
--- src/routes/AppRoute.js
+++ src/routes/AppRoute.js
@@ -1,39 +1,24 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import Spacecraft from '../pages/Spacecraft';
 import SpacecraftBuild from '../pages/SpacecraftBuild';
 import Spacecrafts from '../pages/Spacecrafts';
 import Planets from '../pages/Planets';
-import SpaceTravelApi from '../services/SpaceTravelApi';
-
-import SpacecraftContext from '../context/SpacecraftContext';
+import SpacecraftProvider from '../providers/SpacecraftProvider';
 
 const AppRoute = () => {
-    const INITIAL_STATE = [];
-    const [spacecrafts, setSpacecrafts] = useState(INITIAL_STATE);
-    const buildSpacecraft = (formData) => {
-      setSpacecrafts(crafts => [...crafts, formData]);
-  }
-
-  useEffect(() => {
-    if(spacecrafts.length > 0){
-        const newestSpacecraft = spacecrafts[spacecrafts.length - 1]; 
-        SpaceTravelApi.buildSpacecraft(newestSpacecraft);
-    }
-  }, [buildSpacecraft, spacecrafts])
-
     return (
         <>
-         <SpacecraftContext.Provider value={{spacecrafts, setSpacecrafts, buildSpacecraft}}>
+         <SpacecraftProvider>
             <Routes>
                 <Route path="/" element={<Home />}/>
                 <Route path="spacecraft" element={<Spacecraft />}/>
                 <Route path="/spacecrafts/spacecraftbuild" element={<SpacecraftBuild />}/>
                 <Route path="spacecrafts" element={<Spacecrafts />}/>
                 <Route path="planets" element={<Planets />}/>
-                </Routes>
-            </SpacecraftContext.Provider>
+            </Routes>
+         </SpacecraftProvider>
         </>
     )
 }
