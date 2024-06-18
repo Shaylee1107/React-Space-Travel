25335cf06ff034a6dc3b20f3e75a4d7ca41e6a96
Owner <shayleebaumgartner2002@gmail.com>
2024-06-18 17:35:41 -0400

    refactoring planets

--- src/components/ Planet.js
+++ src/components/ Planet.js
@@ -4,59 +4,84 @@ import '../css/Planet.css';
 
 const Planet = ({ name, pictureUrl, currentPopulation, id }) => {
     const [landedSpacecrafts, setLandedSpacecrafts] = useState([]);
-    console.log(landedSpacecrafts, 'landed')
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         const findLandedSpacecrafts = async () => {
            const spacecrafts = await SpaceTravelApi.getSpacecrafts();
            const spacecraftsData = spacecrafts.data; 
-           spacecraftsData.map((d) => {
-                if(d.currentLocation === id){
-                    return (setLandedSpacecrafts(
-                        landed => [...landed, 
-                            {
-                            'name': d.name, 
-                            'currentLocation': d.currentLocation, 
-                            'id': d.id, 
-                            'key': d.id,
-                            'pictureUrl': d.pictureUrl, 
-                            'capacity': d.capacity
-                            }
-                        ]
-                    ))
-                }
-           });
+           setData(spacecraftsData);
+           showLandedSpacecrafts();
+        //    spacecraftsData.map((d) => {
+        //         if(d.currentLocation === id){
+        //             return (setLandedSpacecrafts(
+        //                 landed => [...landed, 
+        //                     {
+        //                     'name': d.name, 
+        //                     'currentLocation': d.currentLocation, 
+        //                     'id': d.id, 
+        //                     'key': d.id,
+        //                     'pictureUrl': d.pictureUrl, 
+        //                     'capacity': d.capacity
+        //                     }
+        //                 ]
+        //             ))
+        //         }
+        //    });
         }
 
         findLandedSpacecrafts();
     }, [])
-
+   
     const showLandedSpacecrafts = () => {
-        if(landedSpacecrafts.length > 0){
-            return (
-                landedSpacecrafts.map((s) => {
-                    return (
-                        
-                            <div className='img-container'>
-                                <img src={s.pictureUrl} alt="spacecraft" className="img"/>
-                            </div>
-                    
-                    )
-                })
-            )
-        }
-    }
 
+        data.map((d) => {
+                    if(d.currentLocation === id){
+                        return (setLandedSpacecrafts(
+                            landed => [...landed, 
+                                {
+                                'name': d.name, 
+                                'currentLocation': d.currentLocation, 
+                                'id': d.id, 
+                                'key': d.id,
+                                'pictureUrl': d.pictureUrl, 
+                                'capacity': d.capacity
+                                }
+                            ]
+                        ))
+                    }
+               });
+               console.log(landedSpacecrafts, 'landed in showLanded func')
+        // if(landedSpacecrafts.length > 0){
+        //     console.log(landedSpacecrafts, 'landingSpacecrafts')
+        //     return (
+        //         landedSpacecrafts.map((s) => {
+        //             return (
+        //                     <div className='img-container'>
+        //                         <img src={s.pictureUrl} alt="spacecraft" className="img"/>
+        //                     </div>
+        //             )
+        //         })
+        //     )
+        // }
+    }
+    console.log(landedSpacecrafts, 'landedSpacecrafts')
     return (
         <>
-            <div className="container" key={id}>
-                <div className="img-container">
-                    <img src={pictureUrl} className="img"/>
+            <div className="container">
+                <div className="img-container" key={id}>
+                    <img src={pictureUrl} className="img" alt="planet"/>
                     <p>{name}</p>
                 <p>{currentPopulation}</p>
                 </div>
                 <div className="spaceship-container">
-                    {showLandedSpacecrafts()}
+                    {landedSpacecrafts && landedSpacecrafts.map((s) => {
+                    return (
+                            <div className='img-container' key={s.id}>
+                                <img src={s.pictureUrl} alt="spacecraft" className="img"/>
+                            </div>
+                    )
+                })}
                 </div>
             </div>
         </>
--- src/pages/SpacecraftBuild.js
+++ src/pages/SpacecraftBuild.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import SpacecraftContext from '../context/SpacecraftContext';
-import SpaceTravelApi from '../services/SpaceTravelApi';
 
 const SpacecraftBuild = () => {
     const navigate = useNavigate();
