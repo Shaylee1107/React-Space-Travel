87b9dd097642c52e9810ba3e5e180ec2482ec4a4
gerald <maboshegerald1@gmail.com>
2024-06-20 11:51:04 +0200

    fix planet & add gitignore

--- src/components/ Planet.js
+++ src/components/ Planet.js
@@ -3,7 +3,6 @@ import SpaceTravelApi from '../services/SpaceTravelApi';
 import '../css/Planet.css';
 
 const Planet = ({ name, pictureUrl, currentPopulation, id }) => {
-    const [landedSpacecrafts, setLandedSpacecrafts] = useState([]);
     const [data, setData] = useState([]);
 
     useEffect(() => {
@@ -11,61 +10,11 @@ const Planet = ({ name, pictureUrl, currentPopulation, id }) => {
            const spacecrafts = await SpaceTravelApi.getSpacecrafts();
            const spacecraftsData = spacecrafts.data; 
            setData(spacecraftsData);
-           showLandedSpacecrafts();
-        //    spacecraftsData.map((d) => {
-        //         if(d.currentLocation === id){
-        //             return (setLandedSpacecrafts(
-        //                 landed => [...landed, 
-        //                     {
-        //                     'name': d.name, 
-        //                     'currentLocation': d.currentLocation, 
-        //                     'id': d.id, 
-        //                     'key': d.id,
-        //                     'pictureUrl': d.pictureUrl, 
-        //                     'capacity': d.capacity
-        //                     }
-        //                 ]
-        //             ))
-        //         }
-        //    });
         }
 
         findLandedSpacecrafts();
     }, [])
-   
-    const showLandedSpacecrafts = () => {
 
-        data.map((d) => {
-                    if(d.currentLocation === id){
-                        return (setLandedSpacecrafts(
-                            landed => [...landed, 
-                                {
-                                'name': d.name, 
-                                'currentLocation': d.currentLocation, 
-                                'id': d.id, 
-                                'key': d.id,
-                                'pictureUrl': d.pictureUrl, 
-                                'capacity': d.capacity
-                                }
-                            ]
-                        ))
-                    }
-               });
-               console.log(landedSpacecrafts, 'landed in showLanded func')
-        // if(landedSpacecrafts.length > 0){
-        //     console.log(landedSpacecrafts, 'landingSpacecrafts')
-        //     return (
-        //         landedSpacecrafts.map((s) => {
-        //             return (
-        //                     <div className='img-container'>
-        //                         <img src={s.pictureUrl} alt="spacecraft" className="img"/>
-        //                     </div>
-        //             )
-        //         })
-        //     )
-        // }
-    }
-    console.log(landedSpacecrafts, 'landedSpacecrafts')
     return (
         <>
             <div className="container">
@@ -75,13 +24,15 @@ const Planet = ({ name, pictureUrl, currentPopulation, id }) => {
                 <p>{currentPopulation}</p>
                 </div>
                 <div className="spaceship-container">
-                    {landedSpacecrafts && landedSpacecrafts.map((s) => {
-                    return (
-                            <div className='img-container' key={s.id}>
-                                <img src={s.pictureUrl} alt="spacecraft" className="img"/>
-                            </div>
-                    )
-                })}
+
+                {
+                  data.filter((item) => item.currentLocation === id).map((s) => 
+                    (
+                        <div className='img-container' key={s.id}>
+                        <img src={s.pictureUrl} alt="spacecraft" className="img"/>
+                    </div>
+                    ))
+                }
                 </div>
             </div>
         </>
