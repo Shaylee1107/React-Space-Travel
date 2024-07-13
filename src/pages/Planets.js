1fc915e2bdb524f4eb37d4223a436c06991d2ed0
Owner <shayleebaumgartner2002@gmail.com>
2024-07-13 16:04:55 -0400

    changing starting spacecraft iamge

--- src/pages/Planets.js
+++ src/pages/Planets.js
@@ -123,7 +123,7 @@ const Planets = () => {
               description: "Presenting the Astrolux Odyssey: a revolutionary spacecraft merging cutting-edge technology with lavish luxury, designed to usher 10,000 passengers into the solar system's embrace. A marvel of engineering, its sleek exterior is adorned with solar panels, fueling advanced propulsion while minimizing environmental impact." +
                            "Within, the vessel transforms into a haven of opulence. Lavish suites offer cosmic panoramas, celestial artwork bedecks lounges, and sprawling gardens thrive in zero-gravity. Culinary excellence reigns in gourmet restaurants, while immersive theaters and VR chambers offer stellar entertainment." +
                            "Safety remains paramount with cosmic radiation shielding and top-tier medical facilities. The Astrolux Odyssey not only advances space exploration but redefines elegance, uniting humanity's thirst for knowledge with a taste of the sublime.",
-              pictureUrl: null,
+              pictureUrl: "https://png.pngtree.com/png-clipart/20230520/ourmid/pngtree-space-rocket-ship-with-fire-png-image_7103337.png",
               currentLocation: 2
             }
           ]
